3b58e1fa98fe67815cbdcf7aebc3847632997b30
unknown <package9@naver.com>
2020-07-12 15:44:13 +0900

    csv파일 불러오기

---
+++ csv_load.py
@@ -0,0 +1,32 @@
+import functools
+import numpy as np
+import tensorflow as tf
+
+
+TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
+TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
+
+train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
+test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
+np.set_printoptions(precision=3, suppress=True)
+
+LABEL_COLUMN = 'survived'
+# LABELS = [0, 1]
+
+
+def get_dataset(file_path, **kwargs):
+    dataset = tf.data.experimental.make_csv_dataset(
+        file_path,
+        batch_size=5,
+        label_name=LABEL_COLUMN,
+        na_value="?",
+        num_epochs=1,
+        ignore_errors=True,
+        **kwargs)
+    return dataset
+
+
+raw_train_data = get_dataset(train_file_path)
+raw_test_data = get_dataset(test_file_path)
+
+print(raw_test_data)
