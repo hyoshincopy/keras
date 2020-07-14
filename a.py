6c8c31e7af499147ceaa6670223dff9ff91b0b5f
unknown <package9@naver.com>
2020-07-14 23:50:38 +0900

    판다스

--- a.py
+++ a.py
@@ -1,4 +1,27 @@
-from keras.datasets import imdb
+import pandas as pd
+import tensorflow as tf
 
-(train_data, train_labels), (test_data,
-                             test_labels) = imdb.load_data(num_words=10000)
+df = pd.read_csv("./a_12.csv")
+
+print(df.head())
+# import functools
+# import numpy as np
+# import keras
+# import tensorflow as tf
+# model = tf.keras.models.Sequential([
+#     tf.keras.layers.Dense(10, input_shape=[1, 35]),
+#     tf.keras.layers.Dense(10, activation='relu'),
+#     tf.keras.layers.Dense(20, activation='softmax')
+# ])
+
+
+# train_file_path = tf.keras.utils.get_file()
+# test_file_path = tf.keras.utils.get_file("b.csv", TEST_DATA_URL)
+
+# tf.keras.utils
+# model.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+
+
+# model.fit(x_train, y_label, epochs=5)
