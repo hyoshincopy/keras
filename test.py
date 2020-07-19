3606538c92815e157a12356451a810f75f8a6019
unknown <package9@naver.com>
2020-07-19 18:45:16 +0900

    example

--- test.py
+++ test.py
@@ -65,53 +65,53 @@ def transform_features(df):
     return df
 
 
-transform_features(x_train)
+print(transform_features(x_train))
 transform_features(x_test)
 
 #! simplify_ 함수를 통해서 크고 복잡한 값들을 작은 값으로 대체한다
 
-model = keras.models.Sequential()
-# ? input_shape 인 입력 형태인데, 전체 columns의 개수는 6개인데
-# ? 그 중 학습 할 column은 NAME과 BMI를 제외한 4가지 이므로 input_shape=(4,) 가 된다
-model.add(keras.layers.Dense(32, activation='relu', input_shape=(4,)))
-model.add(keras.layers.Dense(32, activation='relu'))
-# model.add(keras.layers.Dense(8, activation='relu'))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
-# ? 출처에서는 RMSprop 옵티마이저를 사용했다
-# model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.001),loss=keras.losses.categorical_crossentropy,metrics=keras.metrics.BinaryAccuracy)
-model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.001),
-              loss=keras.losses.binary_crossentropy,
-              metrics=[keras.metrics.binary_accuracy]
+# model = keras.models.Sequential()
+# # ? input_shape 인 입력 형태인데, 전체 columns의 개수는 6개인데
+# # ? 그 중 학습 할 column은 NAME과 BMI를 제외한 4가지 이므로 input_shape=(4,) 가 된다
+# model.add(keras.layers.Dense(32, activation='relu', input_shape=(4,)))
+# model.add(keras.layers.Dense(32, activation='relu'))
+# # model.add(keras.layers.Dense(8, activation='relu'))
+# model.add(keras.layers.Dense(1, activation='sigmoid'))
+# # ? 출처에서는 RMSprop 옵티마이저를 사용했다
+# # model.compile(optimizer=keras.optimizers.SGD(learning_rate=0.001),loss=keras.losses.categorical_crossentropy,metrics=keras.metrics.BinaryAccuracy)
+# model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=0.001),
+#               loss=keras.losses.binary_crossentropy,
+#               metrics=[keras.metrics.binary_accuracy]
 
-              )
-y_train = np.asarray(y_train)
-x_train = np.asarray(x_train)
-x_test = np.asarray(x_test)
+#               )
+# y_train = np.asarray(y_train)
+# x_train = np.asarray(x_train)
+# x_test = np.asarray(x_test)
 
-# ? validation_size는 batch_size와 같다.(많은 양의 학습 시 몇 개씩 끊어서 할 것인가?)
-# ? 배치사이즈는 학습데이터가 n개가 있다면 최대 n-1개까지 설정가능하다. 그 이상 설정 시 에러
-validation_size = 5
+# # ? validation_size는 batch_size와 같다.(많은 양의 학습 시 몇 개씩 끊어서 할 것인가?)
+# # ? 배치사이즈는 학습데이터가 n개가 있다면 최대 n-1개까지 설정가능하다. 그 이상 설정 시 에러
+# validation_size = 5
 
-x_val = x_train[:validation_size]
-partial_x_train = x_train[validation_size:]
+# x_val = x_train[:validation_size]
+# partial_x_train = x_train[validation_size:]
 
-y_val = y_train[:validation_size]
-partial_y_train = y_train[validation_size:]
+# y_val = y_train[:validation_size]
+# partial_y_train = y_train[validation_size:]
 
-history = model.fit(partial_x_train, partial_y_train,
-                    epochs=30, validation_data=(x_val, y_val))
-acc = history.history['binary_accuracy']
-val_acc = history.history['val_binary_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+# history = model.fit(partial_x_train, partial_y_train,
+#                     epochs=30, validation_data=(x_val, y_val))
+# acc = history.history['binary_accuracy']
+# val_acc = history.history['val_binary_accuracy']
+# loss = history.history['loss']
+# val_loss = history.history['val_loss']
 
-epochs = range(1, len(acc) + 1)
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
+# epochs = range(1, len(acc) + 1)
+# plt.plot(epochs, loss, 'bo', label='Training loss')
+# plt.plot(epochs, val_loss, 'b', label='Validation loss')
 
-plt.legend()
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.show()
+# plt.legend()
+# plt.xlabel('Epochs')
+# plt.ylabel('Loss')
+# plt.show()
 
-#! 좋은 모델과 안좋은 모델 참조) https://kevinthegrey.tistory.com/136
+# #! 좋은 모델과 안좋은 모델 참조) https://kevinthegrey.tistory.com/136
